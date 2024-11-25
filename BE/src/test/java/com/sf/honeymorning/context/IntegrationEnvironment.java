b74c5b7e86660bcc1bdf2692cf44ba6a0f7bca44
kamusari <soccer_by@naver.com>
2024-11-25 23:52:11 +0900

    refactor(test): 빠른 피드백 루프를 유지하면서 테스트의 신뢰성을 높이기 위한 통합 테스트 환경 분리
    이전 대비 실행속도 : 83s -> 25s로 개선
    테스트 비용을 최소화하고 빠르게 오류를 검증하여 문제 지점을 쉽게 찾을 수 있도록 설계

--- BE/src/test/java/com/sf/honeymorning/context/IntegrationEnvironment.java
+++ BE/src/test/java/com/sf/honeymorning/context/IntegrationEnvironment.java
@@ -1,37 +1,10 @@
 package com.sf.honeymorning.context;
 
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
 import com.github.javafaker.Faker;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-public class IntegrationEnvironment {
+public class IntegrationEnvironment{
 	public static final Faker FAKE_DATA_FACTORY = new Faker();
 
-	@Container
-	 public static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0")
-		.withDatabaseName("test_db")
-		.withUsername("test")
-		.withPassword("test")
-		.withReuse(true);
-
-	@DynamicPropertySource
-	static void setDataSourceProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
-		registry.add("spring.datasource.username", mysqlContainer::getUsername);
-		registry.add("spring.datasource.password", mysqlContainer::getPassword);
-	}
 }
---
+++ BE/src/test/java/com/sf/honeymorning/context/database/MySqlContext.java
@@ -0,0 +1,26 @@
+package com.sf.honeymorning.context.database;
+
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public interface MySqlContext {
+    @Container
+    MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("test_db")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(true);
+
+    @DynamicPropertySource
+    static void setDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mysqlContainer::getUsername);
+        registry.add("spring.datasource.password", mysqlContainer::getPassword);
+    }
+}
