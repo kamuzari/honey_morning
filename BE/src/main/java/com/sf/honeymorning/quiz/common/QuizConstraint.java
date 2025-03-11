566ea3eae49e0254f2e6a38d915bef27377d3169
whyWhale <soccer_by@naver.com>
2025-03-11 23:54:31 +0900

    refactor(quiz): 코드퀄리티 향상을 위한 전면 개편
    - 콘텐츠 조회를 이전에는 서버가 렌더링하였음, 서버가 하는일을 분리하여 외부 저장소에 콘텐츠 저장및 접근 가능하도록 하여 이를 제거함
    - stream 파이프라인을 이용하여 entity에서 response로 변경
    - 방어 코드 없는 사용자 답안 제출 비즈니스 로직 보완 강화
    
    - 제거 목록
     - 필요없는 주석
     - 불필요한 로직
     - 휴먼 에러를 동반하는 @Data 어노테이션, setter
     - 무분불변한 Builder, ArgsContstructor 애노테이션 제거
     - 무의미한 제네릭 와일드카드 사용
     - 불필요한 로깅
     - 계층 간 구분 없는 객체 사용
     - 잘못된 비즈니스 로직
     - 무분불한 매직넘버
    
    - 무책임 그 잡채 코드 ... 너무하다.. 증말.. ㅎㄷㄷ

---
+++ BE/src/main/java/com/sf/honeymorning/quiz/common/QuizConstraint.java
@@ -0,0 +1,8 @@
+package com.sf.honeymorning.quiz.common;
+
+public class QuizConstraint {
+    public static final int OPTION_SIZE = 4;
+    public static final int MINIMUM_VALUE = 1;
+    public static final int MAXIMUM_VALUE = OPTION_SIZE;
+    public static final int TOTAL_QUIZ_SIZE = 2;
+}
---
+++ BE/src/main/java/com/sf/honeymorning/quiz/service/mapper/QuizMapper.java
@@ -0,0 +1,21 @@
+package com.sf.honeymorning.quiz.service.mapper;
+
+import com.sf.honeymorning.brief.controller.dto.response.detail.QuizResponseDto;
+import com.sf.honeymorning.quiz.domain.entity.Quiz;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuizMapper {
+    public QuizResponseDto toQuizResponseDto(Quiz quiz) {
+        return new QuizResponseDto(
+                quiz.getProblem(),
+                quiz.getOption1(),
+                quiz.getOption2(),
+                quiz.getOption3(),
+                quiz.getOption4(),
+                quiz.getSelection(),
+                quiz.getAnswer()
+        );
+    }
+
+}
