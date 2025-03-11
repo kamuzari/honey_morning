b4b398aeb3e02b2564248962189257ae6efa9079
whyWhale <soccer_by@naver.com>
2025-03-11 23:41:17 +0900

    refactor(tag): 태그 도메인 알람 도메인 내부로 이동
    - tag 라이프사이클이 알람에 의해 최초로 생성되기 때문에 이동함
    - 일부 브리핑 컨텐츠에서는 복사되어서 사용되어지기 때문에 브리핑 보다는 알람 도메인이 적절하다고 판단하였음

--- BE/src/main/java/com/sf/honeymorning/alarm/domain/repository/AlarmTagRepository.java
+++ BE/src/main/java/com/sf/honeymorning/alarm/domain/repository/AlarmTagRepository.java
@@ -8,17 +8,13 @@ import org.springframework.data.repository.query.Param;
 
 import com.sf.honeymorning.alarm.domain.entity.Alarm;
 import com.sf.honeymorning.alarm.domain.entity.AlarmTag;
-import com.sf.honeymorning.tag.entity.Tag;
+import com.sf.honeymorning.alarm.domain.entity.Tag;
 
 public interface AlarmTagRepository extends JpaRepository<AlarmTag, Long> {
-	List<AlarmTag> findByAlarm(Alarm alarm);
-
 	boolean existsByAlarmAndTag(Alarm alarm, Tag tag);
 
 	@Query("select at from AlarmTag at join fetch at.alarm a join fetch at.tag t where a = :alarm")
 	List<AlarmTag> findByAlarmWithTag(@Param("alarm") Alarm alarm);
 
 	void deleteByAlarmAndTag(Alarm alarm, Tag tag);
-
-	AlarmTag findByTag(Tag tag);
 }
